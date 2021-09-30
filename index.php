42260b30c58df0af0743d38d94e2e416616153fc
SyafiaAzreen <syafiaazreen323@gmail.com>
2021-09-30 10:52:10 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan Kedua</title>
+</head>
+
+<body>
+    <h1>Hello World</h1>
+
+    <?php
+    $nama = 'SYAFIA AZREEN';
+    $umur = 18 + 2;
+
+    echo "$nama BERUMUR $umur tahun";
+    ?>
+</body>
+
+</html>
